7a141185adf0dbc859fa3c78dc835c292a29e5ec
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-06 16:29:39 +0200

    ✨ Allow using a filter

--- src/data-explorer/data-explorer.tsx
+++ src/data-explorer/data-explorer.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useReducer } from "react";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 import { ApolloClient, ApolloError } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
@@ -11,12 +12,12 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-const buildCityQuery = (statID: string) => {
+const buildCityQuery = (statID: string, filter?: string) => {
   return gql`
     query($nuts3: String!) {
       region(id: $nuts3) {
         name
-        stat: ${statID} {
+        stat: ${statID} ${filter ? `(${filter})` : ""} {
           year
           value
         }
@@ -80,13 +81,14 @@ const dataReducer = (state: Data, action): Data => {
 
 const DataExplorer: React.SFC = props => {
   const [statID, setStatID] = useState("");
+  const [filter, setFilter] = useState("");
   const [data, dispatch] = useReducer(dataReducer, {});
   const execute = useCallback(
     async e => {
       e.preventDefault();
       let queryString = "";
       try {
-        const query = buildCityQuery(statID);
+        const query = buildCityQuery(statID, filter);
         queryString = query.loc.source.body;
         const results = await Promise.all(
           Object.entries(cities).map(async ([name, { id }]) => {
@@ -98,8 +100,11 @@ const DataExplorer: React.SFC = props => {
           })
         );
         console.log(results);
-        const lowYear = results.reduce((year, { data }) => {
+        const lowYear = results.reduce((year, { data, name }) => {
           const { stat } = data.region;
+          if (stat.length === 0) {
+            throw new Error(`Statistics for ${name} is empty!`);
+          }
           const lastYear = stat[stat.length - 1].year;
           if (lastYear < year) {
             return lastYear;
@@ -143,7 +148,7 @@ const DataExplorer: React.SFC = props => {
         });
       }
     },
-    [statID]
+    [statID, filter]
   );
 
   const { year, stats, error, query } = data;
@@ -151,34 +156,50 @@ const DataExplorer: React.SFC = props => {
     <Container maxWidth="sm">
       <form onSubmit={execute}>
         <TextField
+          label="ID"
           placeholder="ID der Statistik"
           value={statID}
           onChange={e => setStatID(e.target.value)}
+          variant="outlined"
+          margin="normal"
+          fullWidth
         />
-        {query && (
-          <>
-            <h3>Query:</h3>
-            <pre>{query}</pre>
-          </>
-        )}
-        {error && (
-          <>
-            <h3 style={{ color: "red" }}>Error:</h3>
-            <code>{error}</code>
-          </>
-        )}
-        {stats && year && (
-          <div>
-            <p>Year: {year}</p>
-            <h4>Werte:</h4>
-            {stats.map(({ name, value }) => (
-              <p>
-                {name}: {value}
-              </p>
-            ))}
-          </div>
-        )}
+        <TextField
+          label="Filter"
+          placeholder="leave empty to omit"
+          value={filter}
+          onChange={e => setFilter(e.target.value)}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+        <Button type="submit" variant="outlined">
+          Start Query
+        </Button>
       </form>
+      {error && (
+        <>
+          <h3 style={{ color: "red" }}>Error:</h3>
+          <code>{error}</code>
+        </>
+      )}
+      {stats && year && (
+        <div>
+          <p>Year: {year}</p>
+          <h4>Werte:</h4>
+          {stats.map(({ name, value }) => (
+            <p>
+              {name}: {value}
+            </p>
+          ))}
+        </div>
+      )}
+      {query && (
+        <>
+          <h3>Query:</h3>
+          <pre>{query}</pre>
+        </>
+      )}
     </Container>
   );
 };
