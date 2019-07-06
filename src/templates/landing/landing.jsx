df27e3cdd80cb737559e6f02164d93fac9d42359
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-06 18:54:14 +0200

    🎨 Simplify location data

--- src/templates/landing/landing.jsx
+++ src/templates/landing/landing.jsx
@@ -9,9 +9,9 @@ import Button from '@material-ui/core/es/Button';
 import FormControl from '@material-ui/core/es/FormControl';
 import FormHelperText from '@material-ui/core/es/FormHelperText';
 import Grid from '@material-ui/core/es/Grid';
-import * as locations from '../../assets/locations.json';
+import locations from '../../assets/locations.json';
 
-const sortedLocations = locations.data.allRegions.regions.sort((a, b) => a.name > b.name ? 1 : -1);
+const sortedLocations = locations.sort((a, b) => a.name > b.name ? 1 : -1);
 const Landing = () => {
     const [location, handleLocationChanged]=useFormField('');
     return (
