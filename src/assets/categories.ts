22e4dae7df024adbb381498bbe8058cf625eb466
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-06 19:31:45 +0200

    📝 Document questions in categories

---
+++ src/assets/categories.ts
@@ -0,0 +1,8 @@
+export const categories = {
+    "arbeit-wirtschaft": "Arbeit & Wirtschaft",
+    "bildung": "Bildung",
+    "gesundheit": "Gesundheit",
+    "umwelt": "Umwelt",
+};
+
+export type Category = keyof typeof categories;
--- src/stories/content.stories.tsx
+++ src/stories/content.stories.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { categories } from "../assets/categories";
 
 const req = require.context("../../content", true, /\.js$/);
+const questions: { filename: string; q: ContentDefinition }[] = req
+  .keys()
+  .map(filename => ({ filename, q: req(filename).default }));
 
 interface ContentDefinition {
   content: {
@@ -12,18 +16,25 @@ interface ContentDefinition {
   query: any;
 }
 
-storiesOf("Content", module).add("all", () => {
-  const questions: ContentDefinition[] = req
-    .keys()
-    .map(filename => req(filename).default);
-  console.log(questions);
-  return questions.map(({ content, query }) => (
-    <div>
-      <h2>{content.question}</h2>
-      <p style={{ textTransform: "uppercase", color: "grey" }}>
-        {content.description}
-      </p>
-      <pre>{query.loc.source.body}</pre>
-    </div>
-  ));
+const renderQuestions = ({ q: { content, query } }) => (
+  <div>
+    <h2>{content.question}</h2>
+    <p style={{ textTransform: "uppercase", color: "grey" }}>
+      {content.description}
+    </p>
+    <pre>{query.loc.source.body}</pre>
+  </div>
+);
+
+const stories = storiesOf("Content", module).add("all", () => {
+  return questions.map(renderQuestions);
 });
+
+Object.keys(categories).forEach(cat =>
+  stories.add(categories[cat], () => {
+    const catQuestions = questions.filter(({ filename }) =>
+      filename.startsWith(`./${cat}`)
+    );
+    return catQuestions.map(renderQuestions);
+  })
+);
