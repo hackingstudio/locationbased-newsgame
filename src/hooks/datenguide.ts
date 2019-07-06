5f419e04a3992980ca85b754137167a412576348
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-07 01:46:16 +0200

    🐛 Fix apollo-client in gatsby SSR

--- src/hooks/datenguide.ts
+++ src/hooks/datenguide.ts
@@ -2,10 +2,11 @@ import { GraphQLRequest } from "apollo-link";
 import ApolloClient from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "isomorphic-fetch";
 
 export const client = new ApolloClient({
-  link: createHttpLink({ uri: "https://api-next.datengui.de/graphql" }),
-  cache: new InMemoryCache()
+  link: createHttpLink({ uri: "https://api-next.datengui.de/graphql", fetch }),
+  cache: new InMemoryCache(),
 });
 
 export interface QueryResult {
