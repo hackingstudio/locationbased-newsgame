2c24989fc729f5a0cbb316ad0b425dc92ff32a4f
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-06 23:02:24 +0200

    ✨ Provide game controller

---
+++ src/hooks/controller.ts
@@ -0,0 +1,219 @@
+import { useReducer, useMemo, useCallback } from "react";
+import { Action, createActions } from "./actions";
+import { Category } from "../assets/categories";
+import questions, { Question } from "../assets/questions";
+import { fetchValues } from "./datenguide";
+import { GraphQLRequest } from "apollo-link";
+
+export interface User {
+  name: string,
+  location: string,
+}
+
+export interface QuestionMeta {
+  query: GraphQLRequest,
+  content: Question,
+}
+
+export interface Result {
+  lowYear: number,
+  winner: string,
+  stats: Record<string, number>,
+}
+
+export interface GameState {
+  step: Step,
+  user: User | null,
+  opponent: User | null,
+  category: Category | "",
+  question: QuestionMeta | null,
+  round: number,
+  answers: {
+    self?: string,
+    opponent?: string,
+  }
+  result: Result | null;
+  points: {
+    self: boolean[];
+    opponent: boolean[];
+  }
+}
+
+export enum Step {
+  onboarding, // asks user for name and city
+  opponents,  // searching for opponent
+  category,   // choosing a category
+  question,   // answering a question
+  result,     // seeing who won
+  endOfGame,  // final result
+}
+
+const nextStepMap = {
+  [Step.onboarding]: Step.opponents as Step.opponents,
+  [Step.opponents]: Step.category as Step.category,
+  [Step.category]: Step.question as Step.question,
+  [Step.question]: Step.result as Step.result,
+}
+
+const gameReducer = (state: GameState, action: Action) => {
+  switch (action.type) {
+    case "SET_USER":
+      if (state.step !== Step.onboarding) {
+        return state;
+      }
+      return {
+        ...state,
+        user: action.payload,
+      }
+    case "SET_OPPONENT":
+      if (state.step !== Step.opponents) {
+        return state;
+      }
+      return {
+        ...state,
+        opponent: action.payload,
+      }
+    case "SET_CATEGORY":
+      if (state.step !== Step.category) {
+        return state;
+      }
+      return {
+        ...state,
+        category: action.payload,
+      };
+    case "SET_QUESTION":
+      if (state.step !== Step.question) {
+        return state;
+      }
+      return {
+        ...state,
+        question: action.payload,
+      };
+    case "SET_ANSWER":
+      if (state.step !== Step.question) {
+        return state;
+      }
+      return {
+        ...state,
+        answers: {
+          self: action.payload,
+        },
+      }
+    case "SET_RESULT":
+      if (state.step !== Step.question) {
+        return state;
+      }
+      return {
+        ...state,
+        result: action.payload,
+      }
+    case "ADD_POINTS":
+      if (state.step !== Step.question) {
+        return state;
+      }
+      const { self, opponent } = state.points;
+      return {
+        ...state,
+        points: {
+          self: self.concat(action.payload),
+          opponent,
+        }
+      }
+    case "START_GAME":
+      return {
+        ...state,
+        ...initialGameState,
+        step: Step.opponents,
+      }
+    case "START_ROUND":
+      return {
+        ...state,
+        ...initialRoundState,
+        step: Step.category,
+        round: state.round + 1,
+      }
+    case "NEXT_STEP": {
+      if (!(state.step in nextStepMap)) {
+        return state;
+      }
+      return {
+        ...state,
+        step: nextStepMap[state.step],
+      }
+    }
+    case "RESET":
+      return initialState;
+  }
+}
+
+export const initialRoundState = {
+  category: "" as "",
+  question: null,
+  answers: {} as {
+    self?: string,
+    opponent?: string,
+  },
+  result: null,
+}
+
+export const initialGameState = {
+  user: null,
+  opponent: null,
+  round: -1,
+  points: {
+    self: [],
+    opponent: [],
+  },
+  ...initialRoundState,
+}
+
+export const initialState: GameState = {
+  step: Step.onboarding,
+  ...initialGameState,
+}
+
+export type GameController = typeof gameController;
+
+const gameController = () => {
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+  const { category, question, user, opponent, answers: { self: userAnswer } } = state;
+
+  const {
+    setQuestion,
+    setResult,
+    addPoints,
+    ...actions
+  } = useMemo(() => createActions(dispatch), [dispatch]);
+
+  const findQuestion = useCallback(() => {
+    const list = questions[category];
+    const index = Math.floor(Math.random() * list.length);
+    setQuestion(list[index].content);
+  }, [category, setQuestion]);
+
+  const calculateResult = useCallback(async () => {
+    if (!question || !user || !opponent || !userAnswer) {
+      return;
+    }
+    const { lowYear, stats } = await fetchValues(question.query, [user.location, opponent.location]);
+    if (stats.length < 2) {
+      throw new Error(`Not enough data!`);
+    }
+    const winner = stats.sort((a, b) => a.value - b.value)[0].id;
+    setResult({
+      lowYear,
+      winner,
+      stats: stats.reduce((agg, { id, value }) => (agg[id] = value, agg), {}),
+    });
+    addPoints(winner === userAnswer);
+  }, [question, user, opponent, userAnswer]);
+
+  return {
+    ...actions,
+    findQuestion,
+    calculateResult,
+    ...state,
+  };
+}
+
+export default gameController;
---
+++ src/stories/controller.ts
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { createActions } from "../hooks/actions";
+import { action } from "@storybook/addon-actions";
+import { GameState, initialState, GameController } from "../hooks/controller";
+
+export const mockState = (state: Partial<GameState>): GameState => {
+    return {
+        ...initialState,
+        ...state,
+    }
+}
+
+export const mockGameController: GameController = (state: Partial<GameState> = {}) => {
+    const actions = useMemo(() => createActions(action("action")), [])
+
+    return {
+        ...actions,
+        findQuestion: action("findQuestion"),
+        calculateResult: action("calculateResult"),
+        ...mockState(state),
+    }
+}
