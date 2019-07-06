28679589258ca450d54fd2e1f2741be0ed5ffa19
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-06 17:42:40 +0200

    🎨 Move data explorer into folder

--- src/data-explorer/data-explorer.stories.tsx
+++ src/data-explorer/data-explorer.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import DataExplorer from "./data-explorer";
 
-storiesOf("Data Explorer", module)
+storiesOf("Tools / Data Explorer", module)
   .add("default", () => <DataExplorer />)
   .add("with values", () => (
     <DataExplorer defaultID="STR007" defaultFilter="WZ08RS: WZ08_D" />
