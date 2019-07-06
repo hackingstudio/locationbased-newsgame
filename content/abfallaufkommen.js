290451d7f1941f156a00197ebdffa2500686164e
M4ry666 <52605380+M4ry666@users.noreply.github.com>
2019-07-06 18:25:58 +0200

    Create abfallaufkommen.js

---
+++ content/abfallaufkommen.js
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+export default {
+  query: gql`
+    query($nuts3: String!) {
+      region(id: $nuts3) {
+        name
+        stat: AI1901  {
+          year
+          value
+        }
+      }
+    }
+  `,
+  content: {
+    question: "In welcher Stadt produzieren die Einwohner*innen mehr Müll?",
+    description: "Das Netto-Aufkommen an Haushaltsabfällen pro Einwohner, Elektro-Altgeräte sind davon ausgeschlossen.",
+    unit: "Netto-Abfallaufkommen",
+  },
+};
