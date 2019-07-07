98d1a54be77e0ec55deb028f1b7c1c8e62798737
Christine <christine.gotth@gmail.com>
2019-07-07 02:39:05 +0200

    💄 Add design to results

--- src/templates/results/results.jsx
+++ src/templates/results/results.jsx
@@ -11,49 +11,51 @@ import CountDown from "../../components/countDown";
 import PlayerCard from "../../components/playerCard";
 
 const Results = ({ user, opponent, questions }) => {
-    let winState;
+  let winState;
 
-    const userWon = user.score.filter(x => x).length;
-    const oppWon = opponent.score.filter(x => x).length;
+  const userWon = user.score.filter(x => x).length;
+  const oppWon = opponent.score.filter(x => x).length;
 
-    if (userWon > oppWon) {
-        winState = "win";
-    } else if (userWon < oppWon) {
-        winState = "lost";
-    } else {
-        winState = "draw";
-    }
+  if (userWon > oppWon) {
+    winState = "win";
+  } else if (userWon < oppWon) {
+    winState = "lost";
+  } else {
+    winState = "draw";
+  }
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Typography variant="h3">
-                    {winState === "win" && `Du hast gewonnen!`}
-                    {winState === "lost" && `Du bist Zweite*r!`}
-                    {winState === "draw" && `Unentschieden!`}
-                </Typography>
-            </Grid>
-            <Grid item xs={6}>
-                <Button color="primary" variant="contained" fullWidth>Neues Spiel</Button>
-            </Grid>
-            <Grid item xs={6}>
-                <Button color="secondary" variant="contained" fullWidth disabled>
-                    Menü
-                </Button>
-            </Grid>
-            {questions.map(question => (
-                <Grid item xs={12}>
-                    <Paper>
-                        <Typography variant="h4">{question.question}</Typography>
-                        <Typography variant="body">{question.subquestion}</Typography>
-                        <Typography variant="body">
-                            Mehr Infos: <a href={question.link} />
-                        </Typography>
-                    </Paper>
-                </Grid>
-            ))}
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography variant="h3">
+          {winState === "win" && `Du hast gewonnen!`}
+          {winState === "lost" && `Du bist Zweite*r!`}
+          {winState === "draw" && `Unentschieden!`}
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Button color="primary" variant="contained" fullWidth>
+          Neues Spiel
+        </Button>
+      </Grid>
+      <Grid item xs={6}>
+        <Button color="secondary" variant="contained" fullWidth disabled>
+          Menü
+        </Button>
+      </Grid>
+      {questions.map(question => (
+        <Grid item xs={12}>
+          <Paper>
+            <Typography variant="h4">{question.question}</Typography>
+            <Typography variant="body">{question.subquestion}</Typography>
+            <Typography variant="body">
+              Mehr Infos: <a href={question.link} />
+            </Typography>
+          </Paper>
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
 
 export default Results;
--- src/templates/results/results.stories.jsx
+++ src/templates/results/results.stories.jsx
@@ -3,5 +3,21 @@ import { storiesOf } from "@storybook/react";
 
 import Results from "./results";
 
-storiesOf("Templates / Results", module).add("default", () =>
-<Results user={{ name: "Du", location: "04011" }} opponent={{ name: "Opponent", location: "04012"}} />);
+const question =
+  "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.";
+
+const subQuestion =
+  "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.";
+
+storiesOf("Templates / Results", module).add("default", () => (
+  <Results
+    user={{ name: "Du", location: "04011", score: [true, true, true] }}
+    opponent={{
+      name: "Opponent",
+      location: "04012",
+      score: [true, true, false],
+    }}
+    question={question}
+    subQuestion={subQuestion}
+  />
+));
