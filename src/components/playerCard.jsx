d0ec62227c89080e0d70613c9ed2465386214839
Christine <christine.gotth@gmail.com>
2019-07-07 02:36:45 +0200

    💄 Change styling of player card

--- src/components/playerCard.jsx
+++ src/components/playerCard.jsx
@@ -9,16 +9,16 @@ import unicoat from "../assets/unicoat.png";
 import styles from "./playerCard.module.scss";
 
 const PlayerCard = ({ player }) => (
-    <Paper>
-        <Grid container spacing={1}>
-            <Avatar>{player.name[0].toUpperCase()}</Avatar>
-            <Typography variant="h4" component="h3">
-                {player.name}
-            </Typography>
-        </Grid>
-        <img className={styles.crest} src={unicoat} />
-        <Typography variant="h5">{player.location}</Typography>
-    </Paper>
+  <Paper className={styles.players}>
+    <Grid container spacing={1} className={styles.player}>
+      <Avatar className={styles.avatar}>{player.name[0].toUpperCase()}</Avatar>
+      <Typography variant="h4" component="h3">
+        {player.name}
+      </Typography>
+    </Grid>
+    <img className={styles.crest} src={unicoat} />
+    <Typography variant="h5">{player.location}</Typography>
+  </Paper>
 );
 
 export default PlayerCard;
