05be870c98d8b6b3cbaddc167ea6273ba5d159e5
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-07 02:35:42 +0200

    🎨 Fix code style

--- src/templates/battle/battle.jsx
+++ src/templates/battle/battle.jsx
@@ -1,56 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { } from "react";
 import PropTypes from "prop-types";
 
 import Typography from "@material-ui/core/es/Typography";
-import TextField from "@material-ui/core/es/TextField";
-import Select from "@material-ui/core/es/Select";
-import MenuItem from "@material-ui/core/es/MenuItem";
 import Button from "@material-ui/core/es/Button";
-import LinearProgress from "@material-ui/core/es/LinearProgress";
-import FormControl from "@material-ui/core/es/FormControl";
-import FormHelperText from "@material-ui/core/es/FormHelperText";
 import Grid from "@material-ui/core/es/Grid";
-import Paper from "@material-ui/core/es/Paper";
-import Avatar from "@material-ui/core/es/Avatar";
 import CountDown from "../../components/countDown";
 import PlayerStatus from "../../components/playerStatus";
 
 import { locationsMap } from "../../assets/locations";
-import styles from "./battle.module.scss";
 
 const Battle = ({ question, subQuestion, user, opponent }) => {
-    return (
-        <>
-            <CountDown />
-            <Grid container spacing={3}>
-                <Grid item xs={6}>
-                    <PlayerStatus player={user} />
-                </Grid>
-                <Grid item xs={6}>
-                    <PlayerStatus player={opponent} reverse />
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="h5">{question}</Typography>
-                    <Typography variant="body1">{subQuestion}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button color="primary" variant="contained" fullWidth>
-                        {locationsMap[user.location]}
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button color="primary" variant="contained" fullWidth>
-                        {locationsMap[opponent.location]}
-                    </Button>
-                </Grid>
-            </Grid>
-        </>
-    );
+  return (
+    <>
+      <CountDown />
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <PlayerStatus player={user} />
+        </Grid>
+        <Grid item xs={6}>
+          <PlayerStatus player={opponent} reverse />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5">{question}</Typography>
+          <Typography variant="body1">{subQuestion}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Button color="primary" variant="contained" fullWidth>
+            {locationsMap[user.location]}
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button color="primary" variant="contained" fullWidth>
+            {locationsMap[opponent.location]}
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 Battle.propTypes = {
-    question: PropTypes.string,
-    subQuestion: PropTypes.string
+  question: PropTypes.string,
+  subQuestion: PropTypes.string
 };
 
 export default Battle;
